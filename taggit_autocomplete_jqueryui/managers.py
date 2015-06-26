88884edd6c7952c08a0105ab6a2fbbb1298fe5b4
SReiver <SReiver@users.noreply.github.com>
2015-06-26 15:11:12 +0300

    several fields on one page; fix bug with South

--- taggit_autocomplete_jqueryui/managers.py
+++ taggit_autocomplete_jqueryui/managers.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from taggit.forms import TagField
 from taggit.managers import TaggableManager
+from django.conf import settings
 
 from widgets import TagAutocomplete
 
@@ -15,3 +16,11 @@ class TaggableManagerAutocomplete(TaggableManager):
     def save_form_data(self, instance, value):
         value = map(lambda v: v.strip().lower(), value)
         getattr(instance, self.name).set(*value)
+
+if 'south' in settings.INSTALLED_APPS:
+    try:
+        from south.modelsinspector import add_ignored_fields
+    except ImportError:
+        pass
+    else:
+        add_ignored_fields(["^taggit_autocomplete_jqueryui\.managers"])
