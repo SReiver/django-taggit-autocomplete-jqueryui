3401c549af3f1ad20a23633f165fd95018f1327a
SReiver <SReiver@users.noreply.github.com>
2016-03-15 02:02:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='django-taggit-autocomplete-jqueryui',
-    version='0.3.1dev',
+    version='0.3.2dev',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
