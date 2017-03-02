0df301efe5d295caf142e9fbdfac79ac6fbfafb0
Andrey <andrey.shutkin@gmail.com>
2017-03-02 13:56:21 +0400

    Update urls.py

--- taggit_autocomplete_jqueryui/urls.py
+++ taggit_autocomplete_jqueryui/urls.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import *
+from .views import tag_list_view
 
 urlpatterns = [
-    url(r'^json$', 'taggit_autocomplete_jqueryui.views.tag_list_view',
-            name='taggit_autocomplete_jqueryui_tag_list'),
+    url(r'^json$', tag_list_view, name='taggit_autocomplete_jqueryui_tag_list'),
 ]
+
