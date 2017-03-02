05bb47195dceb1ddf0d3145075ddcd4a531279e9
Andrey <andrey.shutkin@gmail.com>
2017-03-02 13:48:33 +0400

    Django 1.10 compatibility

--- taggit_autocomplete_jqueryui/urls.py
+++ taggit_autocomplete_jqueryui/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import *
 
-urlpatterns = patterns('taggit_autocomplete_jqueryui.views',
+urlpatterns = [
     url(r'^json$', 'tag_list_view',
-            name='taggit_autocomplete_jqueryui_tag_list'),
-)
+            name='taggit_autocomplete_jqueryui.views.taggit_autocomplete_jqueryui_tag_list'),
+]
